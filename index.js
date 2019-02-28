add04ee612f6b5d112a02ff53ffbd814b3a423a7
Jonathan Dorety <kyullog@gmail.com>
2019-02-27 21:34:36 -0500

    Create get request handler with success and failure responses

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const PORT = "2525";
 
 server.use(express.json());
 
+server.get("/api/users", (req, res) => {
+  db.find()
+    .then(users => {
+      res.status(200).json(users);
+    })
+    .catch(err => {
+      req.cancel();
+      res
+        .status(500)
+        .json({ error: "The users information could not be retrieved" });
+    });
+});
+
 server.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}!`);
 });
