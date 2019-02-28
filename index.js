c1ca2fcf969cd4f9ced2d9bfb3f5056713c061ec
Jonathan Dorety <kyullog@gmail.com>
2019-02-27 21:24:16 -0500

    Server is listening

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here
+const express = require("express");
+const db = require("./data/db.js");
+
+const server = express();
+const PORT = "2525";
+
+server.use(express.json());
+
+server.listen(PORT, () => {
+  console.log(`Server is listening at port ${PORT}!`);
+});
