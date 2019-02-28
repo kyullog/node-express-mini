2973b320fe923bde4fc155cde156562cbfc6119a
Jonathan Dorety <kyullog@gmail.com>
2019-02-27 21:59:31 -0500

    Implement basic POST functionality.
    [ ] Need to return created document to response

--- index.js
+++ index.js
@@ -20,6 +20,24 @@ server.get("/api/users", (req, res) => {
     });
 });
 
+server.post("/api/users", (req, res) => {
+  const user = req.body;
+  if (!user.name || !user.bio) {
+    res
+      .status(400)
+      .json({ errorMessage: "Please provide name and bio for the user" });
+  } else {
+    db.insert(user)
+      .then(response => res.status(200).json(response))
+      .catch(err => {
+        req.cancel();
+        res.status(500).json({
+          error: "There was an error while saving the user to the database"
+        });
+      });
+  }
+});
+
 server.listen(PORT, () => {
   console.log(`Server is listening at port ${PORT}!`);
 });
